eadea3dce19f8a6f1cf60a7030f135403162bdf1
Andy Monat <amonat@gmail.com>
2012-04-05 11:58:56 -0400

    Create sample_app with no Test::Unit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7f9caa468abe8a42c58bf159a15766eeb94f0ef1e0916d15aa791de8f175f12ce42c1ceb14f4d022074ccfe056cf2b6df926d4e248d88fc937cafeb6076cf46c'
