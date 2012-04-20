511f8b23c2008b3a23d2359fa78bf3d940c74155
Andy Monat <amonat@gmail.com>
2012-04-19 22:44:50 -0400

    Use a before block to avoid doing visit in every test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,28 +5,26 @@ describe "Static pages" do
   let(:base_title) { $base_title = "Ruby on Rails Tutorial Sample App | " }
 
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the content 'Sample App'" do
-      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the base title" do
-        visit root_path
         page.should have_selector('title',
             :text => "Ruby on Rails Tutorial Sample App")
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "should have the content 'Help'" do
-        visit help_path
         page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
       page.should have_selector('title',
                         :text => base_title + "Help")
     end
@@ -34,13 +32,13 @@ describe "Static pages" do
 
   describe "About page" do
 
+    before { visit about_path }
+
     it "should have the content 'About Us'" do
-        visit about_path
         page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
       page.should have_selector('title',
                     :text => base_title + "About Us")
     end
@@ -48,13 +46,13 @@ describe "Static pages" do
 
   describe "Contact page" do
 
+    before { visit contact_path }
+
     it "should have the content 'Contact'" do
-        visit contact_path
         page.should have_selector('h1', :text => "Contact")
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       page.should have_selector('title',
                     :text => base_title + "Contact")
     end
