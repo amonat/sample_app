153ed2cb1a16897f6ea265d9024019ef9b5a91bc
Andy Monat <amonat@gmail.com>
2012-04-28 13:25:40 -0400

    Add test that User.name shouldn't be empty, and validation to ensure that's the case.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,11 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 end
