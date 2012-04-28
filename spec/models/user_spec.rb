84981b41e8645c1f0dc5a171a9337a17e889eb76
Andy Monat <amonat@gmail.com>
2012-04-28 14:08:46 -0400

    Add tests for various valid email addresses.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,4 +35,14 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
 end
