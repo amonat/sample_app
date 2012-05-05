581952ef9e72a58e9421658db514ab25810a8b1b
Andy Monat <amonat@gmail.com>
2012-05-05 14:45:47 -0400

    Add test to make sure that error messages get shown when you try to create a user with no info filled in.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,12 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "should show error messages" do
+        before { click_button submit }
+        it { should have_selector(".alert-error") }
+        it { should have_content("error") }
+      end
     end
 
     describe "with valid information" do
